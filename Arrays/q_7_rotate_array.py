11ddc25f96ceb4c30e9a94fb87dc7a3bf114e72b
eezhal92 <arkinthesky.69@gmail.com>
2019-06-29 21:23:30 +0800

    array 7th question, rotating array

---
+++ Arrays/q_7_rotate_array.py
@@ -0,0 +1,14 @@
+def rotate(array, n):
+  taken = array[0:n]
+  remaining = array[n:len(array)]
+  array = remaining + taken
+  
+  return array
+  
+t = int(input())
+
+for _ in range(0, t):
+    [_, n] = [int(i) for i in input().split()]
+    array = input().split()
+    rotated_array = rotate(array, n)
+    print(" ".join(rotated_array))
