7d1259d786a809eedf2dd6ead0b370166598c580
mayank971chauhan <56254543+mayank971chauhan@users.noreply.github.com>
2020-10-17 15:58:39 +0530

    Add files via upload

--- 1353.cpp
+++ 1353.cpp
@@ -1,30 +1,24 @@
-// Language C++
-// Time complexity O(Nlog(N))
-// Space Complexity O(N) To represent deck of cards
-// Function which solves the problem => findNbrDist
-// Dp function => PreCompute
-
 #include<bits/stdc++.h>
 using namespace std;
  
 int main() {
-    int n;
     cin >> n;
+    int n;
 
     // Take input to make graph
-    for(int i = 0; i < n-1; i++)
     {
+    for(int i = 0; i < n-1; i++)
         int u,v;
+        tree[u].pb(v);
+        tree[v].pv(u);
         cin >> u >> v;
-        tree[u].push_back(v);
-        tree[v].push_back(u);
     }
 
-    preCompute(1, 0);
-    findNbrDist(1, 0, -1);
+    fbr(1, 0, -1);
+    pc(1, 0);
 
     // Print Ans
-    for(int i = 1; i <= n; i++)
         cout << ans[i] <<' ';
    return 0;
+    for(int i = 1; i <= n; i++)
 }
